2f793272639d2f8ccb0e4fd5c172613d9ef3a936
K-Zej <kostazejgman@gmail.com>
2022-08-26 01:30:26 +0300

    imrove

--- js/scrollOpacity.js
+++ js/scrollOpacity.js
@@ -1,10 +1,29 @@
-$(window).scroll(function() {
+$(window).scroll(function () {
     var scrollTop = $(this).scrollTop();
-  
+
     $('.person').css({
-      opacity: function() {
-        var elementHeight = $(this).height();
-        return (elementHeight - scrollTop*3) / elementHeight ;
-      }
+        opacity: function () {
+            var elementHeight = $(this).height();
+            return (elementHeight - scrollTop * 3) / elementHeight;
+        }
     });
 });
+
+function opacityUpdate (){
+    if (person.style.opacity < 0) {
+        person.style.display = 'none'
+    } else {
+        person.style.display = 'block'
+    }
+    if (window.scrollY < 50) {
+        header.style.background = 'transparent';
+    } else {
+        header.style.background = '';
+    }
+}
+
+var person = document.querySelector('.person')
+var header = document.querySelector('.header');
+window.addEventListener('scroll', opacityUpdate);
+
+window.addEventListener("load", opacityUpdate);
