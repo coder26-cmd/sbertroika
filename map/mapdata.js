1743eda9d8c2adc28cd2523ddbbd3c64feba31f7
K-Zej <kostazejgman@gmail.com>
2022-08-25 17:30:24 +0300

    improve

---
+++ map/mapdata.js
@@ -0,0 +1,351 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "500", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    state_description: "",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    RUS2167: {
+      name: "Томская область",
+      color: "#5ceb92"
+    },
+    RUS2279: {
+      name: "Республика Адыгея"
+    },
+    RUS2280: {
+      name: "Карачаево-Черкесская Республика"
+    },
+    RUS2303: {
+      name: "Республика Ингушетия",
+      color: "#5ceb92"
+    },
+    RUS2304: {
+      name: "Кабардино-Балкарская Республика",
+      color: "#5ceb92"
+    },
+    RUS2305: {
+      name: "Республика Северная Осетия"
+    },
+    RUS2306: {
+      name: "Ставропольский край"
+    },
+    RUS2321: {
+      name: "Chukchi Autonomous Okrug"
+    },
+    RUS2324: {
+      name: "Калининградская область"
+    },
+    RUS2333: {
+      name: "Мурманская область"
+    },
+    RUS2334: {
+      name: "Новгородская область"
+    },
+    RUS2335: {
+      name: "Псковская область"
+    },
+    RUS2336: {
+      name: "Ленинградская область",
+      color: "#5ceb92"
+    },
+    RUS2337: {
+      name: "Санкт-Петербург"
+    },
+    RUS2342: {
+      name: "Брянская область"
+    },
+    RUS2343: {
+      name: "Смоленская область"
+    },
+    RUS2353: {
+      name: "Республика Карелия",
+      color: "#5ceb92"
+    },
+    RUS2354: {
+      name: "Архангельская область"
+    },
+    RUS2355: {
+      name: "Ивановская область"
+    },
+    RUS2356: {
+      name: "Костромская область",
+      color: "#5ceb92"
+    },
+    RUS2357: {
+      name: "Нижегородская область"
+    },
+    RUS2358: {
+      name: "Тверская область"
+    },
+    RUS2359: {
+      name: "Вологодская область"
+    },
+    RUS2360: {
+      name: "Ярославская область"
+    },
+    RUS2361: {
+      name: "Калужская область"
+    },
+    RUS2362: {
+      name: "Курская область",
+      color: "#5ceb92"
+    },
+    RUS2363: {
+      name: "Липецкая область"
+    },
+    RUS2364: {
+      name: "Московская область",
+      color: "#5ceb92"
+    },
+    RUS2365: {
+      name: "Москва",
+      color: "#5ceb92"
+    },
+    RUS2366: {
+      name: "Орловская область",
+      color: "#5ceb92"
+    },
+    RUS2367: {
+      name: "Ростовская область",
+      color: "#5ceb92"
+    },
+    RUS2368: {
+      name: "Тульская область",
+      color: "#5ceb92"
+    },
+    RUS2369: {
+      name: "Волгоградская область"
+    },
+    RUS2370: {
+      name: "Белгородская область"
+    },
+    RUS2371: {
+      name: "Краснодарский край"
+    },
+    RUS2372: {
+      name: "Республика Мордовия"
+    },
+    RUS2373: {
+      name: "Пензенская область"
+    },
+    RUS2374: {
+      name: "Рязанская область"
+    },
+    RUS2375: {
+      name: "Тамбовская область"
+    },
+    RUS2376: {
+      name: "Владимирская область"
+    },
+    RUS2377: {
+      name: "Воронежская область",
+      color: "#5ceb92"
+    },
+    RUS2378: {
+      name: "Республика Башкортостан"
+    },
+    RUS2379: {
+      name: "Челябинская область"
+    },
+    RUS2380: {
+      name: "Курганская область"
+    },
+    RUS2381: {
+      name: "Ненецкий АО"
+    },
+    RUS2382: {
+      name: "Ямало-Ненецкий АО",
+      color: "#5ceb92"
+    },
+    RUS2383: {
+      name: "Республика Коми"
+    },
+    RUS2384: {
+      name: "Кировская область"
+    },
+    RUS2385: {
+      name: "Республика Марий-Эл"
+    },
+    RUS2386: {
+      name: "Свердловская область"
+    },
+    RUS2387: {
+      name: "Республика Удмуртия",
+      color: "#5ceb92"
+    },
+    RUS2388: {
+      name: "Астраханская область"
+    },
+    RUS2389: {
+      name: "Республика Чувашия"
+    },
+    RUS2390: {
+      name: "Республика Калмыкия"
+    },
+    RUS2391: {
+      name: "Оренбургская область"
+    },
+    RUS2392: {
+      name: "Самарская область"
+    },
+    RUS2393: {
+      name: "Саратовская область",
+      color: "#5ceb92"
+    },
+    RUS2394: {
+      name: "Республика Татарстан"
+    },
+    RUS2395: {
+      name: "Ульяновская область"
+    },
+    RUS2396: {
+      name: "Ханты-Мансийский АО"
+    },
+    RUS2397: {
+      name: "Омская область"
+    },
+    RUS2398: {
+      name: "Тюменская область"
+    },
+    RUS2399: {
+      name: "Республика Алтай",
+      color: "#5ceb92"
+    },
+    RUS2400: {
+      name: "Алтайский край"
+    },
+    RUS2401: {
+      name: "Кемеровская область"
+    },
+    RUS2402: {
+      name: "Республика Хакасия"
+    },
+    RUS2403: {
+      name: "Новосибирская область",
+      color: "#5ceb92"
+    },
+    RUS2416: {
+      name: "Республика Чечня"
+    },
+    RUS2417: {
+      name: "Республика Дагестан"
+    },
+    RUS2602: {
+      name: "Иркутская область",
+      color: "#5ceb92"
+    },
+    RUS2603: {
+      name: "Красноярский край"
+    },
+    RUS2605: {
+      name: "Республика Тыва"
+    },
+    RUS2606: {
+      name: "Республика Бурятия"
+    },
+    RUS2609: {
+      name: "Амурская область"
+    },
+    RUS2610: {
+      name: "Забайкальский край"
+    },
+    RUS2611: {
+      name: "Приморский край"
+    },
+    RUS2612: {
+      name: "Республика Саха (Якутия)",
+      color: "#5ceb92"
+    },
+    RUS2613: {
+      name: "Еврейская АО"
+    },
+    RUS2614: {
+      name: "Хабаровский край",
+      color: "#5ceb92"
+    },
+    RUS2615: {
+      name: "Магаданская область"
+    },
+    RUS2616: {
+      name: "Республика Сахалин"
+    },
+    RUS3200: {
+      name: "Пермский край",
+      color: "#5ceb92"
+    },
+    RUS3468: {
+      name: "Камчатский край"
+    }
+  },
+  locations: {},
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
